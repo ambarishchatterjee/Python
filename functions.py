4c07d5aca177e449f62017c4fe5293fc83fd4f97
Ambarish Chatterjee <ambarish@ewaycorp.com>
2019-01-14 22:25:20 +0530

    hw2

---
+++ functions.py
@@ -0,0 +1,21 @@
+#create functions
+def song():
+  print("Fix You")
+
+def Artist():
+    print("Coldplay")
+
+def recorded():
+    print(2004)    
+
+#function with boolean var
+def genre():
+    Rock = bool(1)
+    print(Rock)
+    return
+
+#call functions
+song()
+Artist()
+recorded()
+genre()
