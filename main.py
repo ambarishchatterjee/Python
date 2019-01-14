1b37d343950cfd4b891e7f9d1855d0b51f1b408a
Ambarish Chatterjee <ambarish@ewaycorp.com>
2019-01-14 16:45:47 +0530

    hw1

---
+++ main.py
@@ -0,0 +1,20 @@
+""""
+Task: What's your favorite song?
+Think of all the attributes that you could use to describe that song. That is: all of it's details or "meta-data". These are attributes like "Artist", "Year Released", "Genre", "Duration", etc. Think of as many different characteristics as you can.
+In your text editor, create an empty file and name it main.py
+Now, within that file, list all of the attributes of the song, one after another, by creating variables for each attribute, and giving each variable a value. Here's an example:
+"""
+
+#Assigning variables
+Song = "Fix You"
+Artist = "Coldplay"
+Genre = "Alternative rock"
+Recorded = 2004
+Length = 4.54
+
+#output verables
+print(Song)
+print(Artist)
+print(Genre)
+print(Recorded)
+print(Length)
